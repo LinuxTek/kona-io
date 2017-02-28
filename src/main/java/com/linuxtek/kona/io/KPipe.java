8343422bfc38dfd6081dee89f6fdfea9efbd5527
Sharif Alexandre <sharif@fabriq.io>
2017-02-28 12:23:57 -0500

    updates

--- src/main/java/com/linuxtek/kona/io/KPipe.java
+++ src/main/java/com/linuxtek/kona/io/KPipe.java
@@ -3,10 +3,16 @@
  */
 package com.linuxtek.kona.io;
 
+
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class KPipe implements Runnable {
+	private static Logger logger = LoggerFactory.getLogger(KPipe.class);
 
 	private Thread t = null;
 	private InputStream in = null;
@@ -18,12 +24,13 @@ public class KPipe implements Runnable {
 		this.in = in;
 		this.out = out;
 		//chars = new char[1024];
+        
 		t = new Thread(this);
 		t.start();
 	}
 
 	public int getCount() {
-		return (count);
+		return count;
 	}
 
 	public void stop() {
@@ -38,6 +45,7 @@ public class KPipe implements Runnable {
 			//	return;
 	
 			int c = in.read();	
+            
 			while (c != -1 && t == current) {
 				//chars[count] = (char) c;
 				count++;
@@ -46,6 +54,7 @@ public class KPipe implements Runnable {
 			}
 		}
 		catch (Exception e) { 
+            logger.error(e.getMessage(), e);
         } finally {
 			//String s = new String(chars);
 			//System.err.println("------------- CHARS -------\n" +s);
